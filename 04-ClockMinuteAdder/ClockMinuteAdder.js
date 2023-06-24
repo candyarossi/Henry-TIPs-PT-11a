55fc45210f3c45a66f546d4af7b19fad9c51b96a
Candela Yarossi <candyarossi@gmail.com>
2023-06-23 23:40:56 -0300

    ClockMinuteAdder + Intersection

--- 04-ClockMinuteAdder/ClockMinuteAdder.js
+++ 04-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,5 +1,38 @@
 function clockMinuteAdder(time, minutesToAdd) {
   // Tu código aquí
+
+  // Opción 1
+
+  let arrTime = time.split(":");
+  let horas = Number(arrTime[0]);
+  let minutos = Number(arrTime[1]);
+
+  minutos += minutesToAdd;
+
+  if (minutos >= 60) {
+    horas = horas + Math.floor(minutos / 60);
+    minutos = minutos % 60;
+    horas > 12 && horas % 12 === 0 ? (horas = 12) : (horas = horas % 12);
+  }
+
+  horas = horas < 10 ? "0" + horas : horas;
+  minutos = minutos < 10 ? "0" + minutos : minutos;
+
+  return horas + ":" + minutos;
+
+  // Opción 2
+
+  let [hours, minutes] = time.split(":");
+
+  minutes = Number(minutes) + minutesToAdd;
+  hours = Number(hours) + Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  hours = ((hours - 1) % 12) + 1;
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  return hours + ":" + minutes;
 }
 
 module.exports = clockMinuteAdder;
--- 05-Intersection/Intersection.js
+++ 05-Intersection/Intersection.js
@@ -1,5 +1,17 @@
 function intersection(arr1, arr2) {
   // Tu código aquí
+
+  // Opción 1
+
+  let arrDup = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) arrDup.push(arr1[i]);
+  }
+  return arrDup;
+
+  // Opción 2
+
+  return arr1.filter((e) => arr2.includes(e));
 }
 
 module.exports = intersection;
