bcefae3e4601ea22ac53105494cab13efba1cbbc
Candela Yarossi <candyarossi@gmail.com>
2023-06-27 21:38:27 -0300

    SpyOn

--- 07-SpyOn/SpyOn.js
+++ 07-SpyOn/SpyOn.js
@@ -1,5 +1,45 @@
 function spyOn(fn) {
   // Tu código aquí
+
+  // Opción 1 -> con Arrays
+
+  let count = 0;
+  let cacheValues = [];
+  let cacheResults = [];
+
+  const spy = (...args) => {
+    count++;
+    for (const v of args) cacheValues.push(v);
+    let result = fn(...args);
+    cacheResults.push(result);
+    return result;
+  };
+
+  spy.getCallCount = () => count;
+  spy.wasCalledWith = (value) => cacheValues.includes(value);
+  spy.returned = (value) => cacheResults.includes(value);
+
+  return spy;
+
+  // Opción 2 -> con Objetos
+
+  let count = 0;
+  let cacheValues = {};
+  let cacheResults = {};
+
+  const spy = (...args) => {
+    count++;
+    for (const v of args) cacheValues[v] = true;
+    let result = fn(...args);
+    cacheResults[result] = true;
+    return result;
+  };
+
+  spy.getCallCount = () => count;
+  spy.wasCalledWith = (value) => !!cacheValues[value];
+  spy.returned = (value) => !!cacheResults[value];
+
+  return spy;
 }
 
 module.exports = spyOn;
